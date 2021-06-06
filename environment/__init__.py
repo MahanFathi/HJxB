364204b1862f14c5ab8c665808ca479bfb6f9dcb
MahanFathi <mahan0fathi@gmail.com>
2021-06-06 18:52:52 -0400

    Add and refactor derivative tests

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,13 +1,14 @@
 from .core import System, Env
-from .envs.pendulum import PendulumSys, PendulumEnv
+from .envs.dummy import DummySys, DummyEnv
 from .envs.double_integrator import DoubleIntegratorSys, DoubleIntegratorEnv
+from .envs.pendulum import PendulumSys, PendulumEnv
 from yacs.config import CfgNode
 
 __all__ = ["System", "Env"]
 
-__all__ += ["PendulumSys", "PendulumEnv"]
-
+__all__ += ["DummySys", "DummyEnv"]
 __all__ += ["DoubleIntegratorSys", "DoubleIntegratorEnv"]
+__all__ += ["PendulumSys", "PendulumEnv"]
 
 def make_env(cfg: CfgNode):
     return globals()[cfg.ENV.ENV_NAME](h=cfg.ENV.TIMESTEP)
--- tests/tests.py
+++ tests/tests.py
@@ -1,29 +1,96 @@
 import unittest
 
 import jax
-from jax import numpy as jnp
+from jax import numpy as jnp, test_util
 
-from environment.envs.dummy import DummySys, DummyEnv
+import numpy as np
+import scipy as sp
+
+from config.defaults import get_cfg_defaults
+from environment import make_env
+from algo import make_algo
 
 class TestDerivatives(unittest.TestCase):
 
     def setUp(self, ):
         self.rng = jax.random.PRNGKey(2021)
-        self.env = DummyEnv()
-        self.sys = self.env.sys
+        self.cfg = get_cfg_defaults()
 
-    def test_derivatives(self, ):
+    def test_jacobians_dummy(self, ):
+        """ Also checks if jacobians
+            are implemented correctly.
+        """
+        self.cfg.merge_from_list([
+            "ENV.ENV_NAME", "DummyEnv",
+        ])
+        env = make_env(self.cfg)
+        sys = env.sys
+        obs_size = env.observation_space.shape[0]
+        act_size = env.action_space.shape[0]
+
         N = 4
         self.rng, subkey = jax.random.split(self.rng)
-        x = jax.random.normal(subkey, (N, self.sys.obs_size))
-        u = jax.random.normal(subkey, (N, self.sys.act_size))
+        x = jax.random.normal(subkey, (N, obs_size))
+        u = jax.random.normal(subkey, (N, act_size))
 
-        jac_f_x, jac_f_u = self.sys.jac_f_fn(x, u)
-        self.assertTrue(jnp.all(jnp.equal(jac_f_x, self.sys.A)), "f_x jac failed.")
-        self.assertTrue(jnp.all(jnp.equal(jac_f_u, self.sys.B)), "f_u jac failed.")
+        jac_f_x, jac_f_u = sys.jac_f_fn(x, u)
+        self.assertTrue(jnp.all(jnp.equal(jac_f_x, sys.A)), "f_x jac failed.")
+        self.assertTrue(jnp.all(jnp.equal(jac_f_u, sys.B)), "f_u jac failed.")
 
-        hess_g_u = self.sys.hess_g_u_fn(x, u)
-        self.assertTrue(jnp.all(jnp.equal(hess_g_u, self.sys.R + self.sys.R.T)), "g_u hess failed.")
+        hess_g_u = sys.hess_g_u_fn(x, u)
+        self.assertTrue(jnp.all(jnp.equal(hess_g_u, sys.R + sys.R.T)), "g_u hess failed.")
+
+        test_util.check_grads(sys.f_fn, (x, u), order=1)
+        test_util.check_grads(sys.g_fn, (x, u), order=2)
+
+    def test_jacobians_pendulum(self, ):
+        self.cfg.merge_from_list([
+            "ENV.ENV_NAME", "PendulumEnv",
+        ])
+        env = make_env(self.cfg)
+        sys = env.sys
+        obs_size = env.observation_space.shape[0]
+        act_size = env.action_space.shape[0]
+
+        N = 4
+        self.rng, subkey = jax.random.split(self.rng)
+        x = jax.random.normal(subkey, (N, obs_size))
+        u = jax.random.normal(subkey, (N, act_size))
+
+        test_util.check_grads(sys.f_fn, (x, u), order=1)
+        test_util.check_grads(sys.g_fn, (x, u), order=2)
+
+        # TODO(mahan): check if derivatives are close numerically
+        # def f_x_jvp(x, v):
+        #     x, v = tuple(x)[0], tuple(v)[0]
+        #     f = sys.f_fn(x, u)
+        #     jvp = sys.jac_f_fn(x, u)[0] @ v
+        #     return f, jvp
+
+        # NOTE(mahan): batch'd jacs should be handled
+        # print(test_util.check_jvp(
+        #     lambda x: sys.f_fn(x),
+        #     f_x_jvp, x
+        # ))
+
+    def test_perturbation(self, ):
+
+        self.cfg.merge_from_list([
+            "TRAIN.ALGO_NAME", "DPcFVI",
+            "ENV.ENV_NAME", "PendulumEnv",
+            "TRAIN.DATASET_SIZE", 4,
+            "TRAIN.BATCH_SIZE", 1,
+        ])
+        self.cfg.freeze()
+
+        env = make_env(self.cfg)
+        algo = make_algo(self.cfg, env)
+        x_dataset = algo.get_x_train()
+        j_dataset = algo.get_target_j(x_dataset)
+        loss_fn = lambda params: algo.loss_and_grad(x_dataset, j_dataset, params)[0]
+        loss_grad_fn = lambda params: algo.loss_and_grad(x_dataset, j_dataset, params)[1]
+        params = algo.optimizer.target
+        test_util.check_grads(loss_fn, (params, ), order=2)
 
 
 if __name__ == '__main__':
