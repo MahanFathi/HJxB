ab2244d3c998dfd2aa06a8156600843f955066e2
d3sm0 <simone.totaro@gmail.com>
2021-07-07 04:41:49 +0200

    Hello buddy. Change `cfg.HOST=""` for local run. Follow the readme here: `https://github.com/ministry-of-silly-code/experiment_buddy` on how to set your ssh config and make sure the cluster can pull from git.

--- main.py
+++ main.py
@@ -2,32 +2,14 @@ from config.defaults import get_cfg_defaults
 from environment import make_env
 from algo import make_algo
 
-import argparse
+import experiment_buddy
 
 
 def main():
-    parser = argparse.ArgumentParser(description="HJxB")
-    parser.add_argument(
-        "--config-file",
-        default="",
-        metavar="file",
-        help="path to yaml config file",
-        type=str,
-    )
-    parser.add_argument(
-        "--opts",
-        help="Modify config options using the command-line",
-        default=[],
-        nargs=argparse.REMAINDER,
-    )
-
-    args = parser.parse_args()
-
     # build the config
     cfg = get_cfg_defaults()
-    cfg.merge_from_file(args.config_file)
-    cfg.merge_from_list(args.opts)
-    cfg.freeze()
+    experiment_buddy.register(cfg)
+    # cfg.freeze()
 
     env = make_env(cfg)
     algo = make_algo(cfg, env)
